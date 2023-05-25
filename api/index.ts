a4d02d3940550f3aca786aad95f800fb01168641
stan.gao <stan.gao@tracade.cn>
2023-05-25 11:41:17 +0800

    init

---
+++ api/index.ts
@@ -0,0 +1,11 @@
+export const getCharacterInfo = (userName: string) => {
+	return uni.request({
+		url: `https://api.maplestory.gg/v2/public/character/gms/${userName}`
+	})
+}
+
+export const getAutoCompleteList = (input: string) => {
+	return uni.request({
+		url: `https://api.maplestory.gg/v1/public/autocomplete/${input}/gms`
+	})
+}
---
+++ pages/characterSearch/characterSearch.vue
@@ -0,0 +1,31 @@
+<template>
+	<view class="character-search-container">
+		<uni-easyinput placeholder="角色名称" v-model="inputValue"></uni-easyinput>
+		<view class="auto-complete-list-container">
+			<view class="auto-complete-item"></view>
+		</view>
+	</view>
+</template>
+
+<script setup lang="ts">
+	import { watch, ref } from 'vue';
+	import {
+		getCharacterInfo,
+		getAutoCompleteList
+	} from "../../api";
+
+	const inputValue = ref("")
+
+	watch(inputValue, () => {
+		console.log(123)
+		getAutoCompleteList(inputValue.value).then(res => {
+			console.log(res)
+		}).catch(e => {
+			console.error(e)
+		})
+	})
+</script>
+
+<style lang="less">
+
+</style>
